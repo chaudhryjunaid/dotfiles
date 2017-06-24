7177ab212aa4e5d0e5a259fb5b6871cb0d10f697
Chaudhry Junaid Anwar <junaidanwar10@gmail.com>
2017-06-24 18:32:48 +0500

    install dotfiles in location

--- install.sh
+++ install.sh
@@ -5,10 +5,15 @@ set -e
 set -o pipefail
 
 
-ln -s ./vim/vimrc ~/.vimrc
-ln -s ./vim/gvimrc ~/.gvimrc
-ln -s ./git/gitexcludes ~/.gitexcludes
-ln -s ./git/gitconfig ~/.gitconfig
+#curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+#	    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+ln -sv ./vim/vimrc ~/.vimrc
+ln -sv ./vim/gvimrc ~/.gvimrc
+
+
+ln -sv ./git/gitexcludes ~/.gitexcludes
+ln -sv ./git/gitconfig ~/.gitconfig
 
 exit 0;
 

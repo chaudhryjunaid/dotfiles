f8a072074673520bf5597dba7f99b26bad778565
Chaudhry Junaid Anwar <junaidanwar10@gmail.com>
2017-07-29 18:59:51 +0500

    add semi-cooked config files

---
+++ brew.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+
+# Homebrew
+#
+# This installs some of the common dependencies needed (or at least desired)
+# using Homebrew.
+
+# Check for Homebrew
+if test ! $(which brew)
+then
+  echo "  Installing Homebrew for you."
+
+  # Install the correct homebrew for each OS type
+  if test "$(uname)" = "Darwin"
+  then
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  elif test "$(expr substr $(uname -s) 1 5)" = "Linux"
+  then
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
+  fi
+
+fi
+
+xcode-select --install
+
+# Make sure we’re using the latest Homebrew.
+brew update
+
+# Upgrade any already-installed formulae.
+brew upgrade
+
+brew tap homebrew/bundle
+brew bundle
+
+# Switch to using brew-installed bash as default shell
+if ! fgrep -q '/usr/local/bin/zsh' /etc/shells; then
+  echo '/usr/local/bin/zsh' | sudo tee -a /etc/shells;
+  chsh -s /usr/local/bin/zsh;
+fi;
+
+# Remove outdated versions from the cellar.
+brew cleanup
+
+echo "export PATH=$(brew --prefix coreutils)/libexec/gnubin:$PATH" >>~/.zshrc
+

410c5e34007d4ebdba48a2827646a7d2f83d7b52
Chaudhry Junaid Anwar <junaidanwar10@gmail.com>
2017-07-29 23:29:30 +0500

    cleanup backup-machine.sh

--- backup-machine.sh
+++ backup-machine.sh
@@ -1,18 +1,23 @@
+#!/bin/sh
+
+set -e
+
 ###  backup old machine's key items
 
-mkdir -p ~/migration/home/
-mkdir -p ~/migration/Library/"Application Support"/
-mkdir -p ~/migration/Library/Preferences/
-mkdir -p ~/migration/Library/Application Support/
-mkdir -p ~/migration/rootLibrary/Preferences/SystemConfiguration/
+mkdir -p ~/backup
+mkdir -p ~/backup/mac
+mkdir -p ~/backup/mac/home
+mkdir -p ~/backup/mac/Library/"Application Support"/
+mkdir -p ~/backup/mac/Library/Preferences/
+mkdir -p ~/backup/mac/rootLibrary/Preferences/SystemConfiguration/
 
-cd ~/migration
+cd ~/backup/mac
 
 # what is worth reinstalling?
 brew leaves              > brew-list.txt    # all top-level brew installs
 brew cask list           > cask-list.txt
 npm list -g --depth=0    > npm-g-list.txt
-yarn global ls --depth=0 > yarn-g-list.txt
+#yarn global ls --depth=0 > yarn-g-list.txt
 
 # then compare brew-list to what's in `brew.sh`
 #   comm <(sort brew-list.txt) <(sort brew.sh-cleaned-up)
@@ -26,47 +31,29 @@ cp -Rp \
     ~/.netrc \
     ~/.ssh \
     ~/.z   \
-        ~/migration/home
+    ~/backup/mac/home
 
-cp -Rp ~/Documents ~/migration
+#cp -Rp ~/Documents ~/backup/mac
+#cp -Rp ~/Pictures ~/backup/mac
+#cp -Rp ~/Downloads ~/backup/mac
 
-cp -Rp /Library/Preferences/SystemConfiguration/com.apple.airport.preferences.plist ~/migration/rootLibrary/Preferences/SystemConfiguration/ # wifi
+cp -Rp /Library/Preferences/SystemConfiguration/com.apple.airport.preferences.plist ~/backup/mac/rootLibrary/Preferences/SystemConfiguration/ # wifi
 
-cp -Rp ~/Library/Services ~/migration/Library/ # automator stuff
-cp -Rp ~/Library/Fonts ~/migration/Library/ # all those fonts you've installed
+cp -Rp ~/Library/Services ~/backup/mac/Library/ # automator stuff
+cp -Rp ~/Library/Fonts ~/backup/mac/Library/ # all those fonts you've installed
 
 # editor settings & plugins
-cp -Rp ~/Library/Application\ Support/Sublime\ Text\ * ~/migration/Library/"Application Support"
+cp -Rp ~/Library/Application\ Support/Sublime\ Text\ * ~/backup/mac/Library/"Application Support"
 
 # also consider...
 # random git branches you never pushed anywhere?
 # git untracked files (or local gitignored stuff). stuff you never added, but probably want..
-
-
 # OneTab history pages, because chrome tabs are valuable.
-
 # usage logs you've been keeping.
-
 # iTerm settings.
   # Prefs, General, Use settings from Folder
-
-# Finder settings and TotalFinder settings
-#   Not sure how to do this yet. Really want to.
-
-# Timestats chrome extension stats
-#   chrome-extension://ejifodhjoeeenihgfpjijjmpomaphmah/options.html#_options
-# 	gotta export into JSON through devtools:
-#     copy(JSON.stringify(localStorage))
-#     pbpaste > timestats-canary.json.txt
-
 # software licenses.
 #   sublimetext's is in its Application Support folder
 
-# maybe ~/Pictures and such
-cp -Rp ~/Pictures ~/migration
-
 ### end of old machine backup
-##############################################################################################################
-
-
 
